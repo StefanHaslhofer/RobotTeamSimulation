3e65d28a573e77c26014c5f6d2d48ca46b490f50
StefanHaslhofer <haslhofer.stefan@gmail.com>
2024-05-15 23:39:47 +0200

    add zones and force variables

--- model/agent.py
+++ model/agent.py
@@ -10,19 +10,33 @@ def normalize_vec(vec: Tuple[int, int]):
 
 
 class Agent:
-    def __init__(self, x, y, direction: Tuple[int, int]):
+    def __init__(self, x, y, direction: Tuple[int, int], r_zone, o_zone, a_zone):
         self.x = x
         self.y = y
-        self.direction = normalize_vec(direction)
+        self._direction = normalize_vec(direction)
+        self.r_zone = r_zone  # repulsion zone radius
+        self.o_zone = o_zone  # orientation zone radius
+        self.a_zone = a_zone  # attraction zone radius
+        self.f_r = 0  # repulsion force
+        self.f_o = 0  # orientation force
+        self.f_a = 0  # attraction force
 
-    def tick(self):
-        self.x += self.direction[0]
-        self.y += self.direction[1]
+    def tick(self, agents, predators):
+        """
+        perform a simulation step
+
+        1. search for agents within each zones
+        2. calculate forces on the agent exerted by all other agents and predators
+        3. adapt direction
+        4. update position based on direction
+        """
+        self.x += self._direction[0]
+        self.y += self._direction[1]
 
     @property
     def direction(self) -> Tuple[int, int]:
-        return self.direction
+        return self._direction
 
     @direction.setter
     def direction(self, vec: Tuple[int, int]):
-        self.direction = normalize_vec(vec)
+        self._direction = normalize_vec(vec)
