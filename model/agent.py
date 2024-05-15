5fb59ff7d217e39777a93bf01586a62085c0bec5
StefanHaslhofer <haslhofer.stefan@gmail.com>
2024-05-15 23:14:16 +0200

    put agent in agent.py

--- model/agent.py
+++ model/agent.py
@@ -1,11 +1,19 @@
 from typing import Tuple
+import numpy as np
+
+
+def normalize_vec(vec):
+    norm = np.linalg.norm(vec)
+    if norm == 0:
+        return vec
+    return vec / norm
 
 
 class Agent:
     def __init__(self, x, y, direction: Tuple[int, int]):
         self.x = x
         self.y = y
-        self.direction = direction
+        self.direction = normalize_vec(direction)
 
     def tick(self):
         self.x += self.direction[0]
