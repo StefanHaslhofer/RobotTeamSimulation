3d18c82e0b40c90d98df5785ad5b0b0e2fcb8199
StefanHaslhofer <haslhofer.stefan@gmail.com>
2024-06-12 01:32:59 +0200

    only change direction once per tick

--- model/agent.py
+++ model/agent.py
@@ -12,19 +12,15 @@ class Agent:
         self.speed = speed
 
     def tick(self, agents, predators, tasks):
-        # change position along direction vector
-        self.x += self.direction[0] * self.speed
-        self.y += self.direction[1] * self.speed
         center = (MAP_SIZE_X/2 - self.x, MAP_SIZE_Y/2 - self.y)
         # reflect movement at the edge of the map to the center
         if self.x < MAP_INNER_BORDER or self.x > MAP_SIZE_X - MAP_INNER_BORDER:
             self.direction = center
-            # add a small random factor to x movement
-            self.x += self.direction[0]
         if self.y < MAP_INNER_BORDER or self.y > MAP_SIZE_Y - MAP_INNER_BORDER:
             self.direction = center
-            # add a small random factor to y movement
-            self.y += self.direction[1]
+        # change position along direction vector
+        self.x += self.direction[0] * self.speed
+        self.y += self.direction[1] * self.speed
         # cap movement at edge of map
         # self.x = min(MAP_SIZE_X, max(0, self.x))
         # self.y = min(MAP_SIZE_Y, max(0, self.y))
